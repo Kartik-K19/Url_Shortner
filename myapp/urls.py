2797cc0ebf5a815309586d9e3503e68d1dd89cf1
Tejashri Lonkar <tejashrilonkar20@gmail.com>
2022-04-05 00:14:59 +0530

    make it better

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('hello', views.hello_world),
+    path('', views.home_page),
+    path('test', views.test),
+    path('all-analytics', views.all_analytics),
+
+    path('<slug:short_url>', views.redirect_url)
+]
+
